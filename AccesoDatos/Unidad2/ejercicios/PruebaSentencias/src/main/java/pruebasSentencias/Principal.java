a332da557d342879d040006b4c831bd16c486372
Fatima <fatima.mortahilchachou@riberadeltajo.es>
2024-10-18 21:24:55 +0200

    18/10/2024  COMMIT
    Additions:
    	- I created the method "insertarEmpleados", exercise (pruebasSentencias) still unfinished

--- AccesoDatos/Unidad2/ejercicios/PruebaSentencias/src/main/java/pruebasSentencias/Principal.java
+++ AccesoDatos/Unidad2/ejercicios/PruebaSentencias/src/main/java/pruebasSentencias/Principal.java
@@ -10,7 +10,129 @@ import java.sql.*;
 
 public class Principal {
 	
+	
 	public static void main(String[] args) {
+	
+		Connection conexion=Conexiones.getMysql("ejemplo", "root","1234");
+
+		System.out.println("PRUEBA verempleados MYSQL");
+		
+		//error en empleado, dir y dep 
+		System.out.println(insertarEmpleado(conexion,7369,"","",222,-1500,100,45));
+		System.out.println("----------------------");
+		// error en el empleado 
+		System.out.println(insertarEmpleado(conexion,7369,"EMPLE123","INFORMATICO",7499,1500,100,10));
+		System.out.println("----------------------");
+		//error en dep
+		System.out.println(insertarEmpleado(conexion,123,"EMPLE123",null,7499,1500,100,45));
+		System.out.println("----------------------");
+		//no hay error
+		System.out.println(insertarEmpleado(conexion,123,"EMPLE123","INFORMATICO",7499,1500,100,10));
+
+		
+		
+	}
+	
+	private static String insertarEmpleado(Connection conexion, int emp_no, String apellido,String oficio, int dir , float salario, float comision, int dept_no) {
+
+		String mensaje="";
+		int error =0; // si ocurre error la ponemos a 1
+		
+		if(salario<=0) {
+			error=1;
+			mensaje=mensaje+"EL SALARIO ES NEGATIVO, ERROR NO PUEDE SER NEGATIVO.\n";
+		}
+		
+		if(apellido==null || apellido.equals("")) {
+			error=1;
+			mensaje=mensaje+"EL APELLIDO NO PUEDE SER NULO.\n";
+		}
+		
+		if(oficio==null || oficio.equals("")) {
+			error=1;
+			mensaje=mensaje+"EL OFICIO NO PUEDE SER NULO.\n";
+		}
+		
+		//Comprobar si el empleado existe
+		String sql1="select count(*) from empleados where emp_no="+emp_no;
+		Statement sentencia;
+		try {
+			sentencia=conexion.createStatement();
+			ResultSet resul=sentencia.executeQuery(sql1);
+			resul.next();
+			int cuenta=resul.getInt(1);
+			if(cuenta==1) {
+				//empleado existe, no se puede insertar
+				mensaje=mensaje+"EL NUM DE EMPLEADO YA EXISTE,"+emp_no+", ERROR AL INSERTAR.\n";
+				error=1;
+			}
+			
+			//Comprobar si el director existe 
+			 sql1="select count(*) from empleados where emp_no="+dir;
+			sentencia=conexion.createStatement();
+			resul=sentencia.executeQuery(sql1);
+			resul.next();
+			cuenta=resul.getInt(1);
+			if(cuenta==0) {
+				//director no existe
+				mensaje=mensaje+"EL DIRECTOR ("+dir+") NO EXISTE EN EMPLEADOS, ERROR AL INSERTAR.\n";
+				error=1;
+			}
+			
+			//Comprobar si el dep existe 
+			
+			 sql1="select count(*) from departamentos where dept_no="+dept_no;
+				sentencia=conexion.createStatement();
+				resul=sentencia.executeQuery(sql1);
+				resul.next();
+				cuenta=resul.getInt(1);
+				if(cuenta==0) {
+					//director no existe
+					mensaje=mensaje+"EL NUM DE DEPARTAMENTO("+dept_no+") NO EXISTE EN LA TABLA DEPARTAMENTOS.\n";
+					error=1;
+				}
+				
+				//Una vez finalizadas las comprobaciones se pregunta si ha habido algún error
+				if(error==1) {
+					//no se inserta
+					mensaje=mensaje+"REGISTRO NO INSERTADO";
+				}else {
+					//A insertar el empleado
+				//	mensaje=mensaje+"DATOS CORRECTOS. A INSERTAR";
+					
+					mensaje=insertaremple( conexion,  emp_no, 
+							 apellido, oficio,  dir ,  salario, 
+							 comision,  dept_no);
+				}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return mensaje;
+	}
+	
+	
+	private static String insertaremple(Connection conexion, int emp_no, 
+			String apellido,String oficio, int dir , float salario, 
+			float comision, int dept_no) {
+		String mensaje="";
+		//cargar fecha 
+		java.util.Date utilDate=new java.util.Date();
+		java.sql.Date sqlDate=new java.sql.Date(utilDate.getTime());
+		System.out.println(utilDate);
+		System.out.println(sqlDate);
+		
+	return mensaje;
+	}
+
+	
+	
+	
+
+	public static void verempleados(String[] args) {
 		Connection conexion=Conexiones.getMysql("ejemplo", "root","1234");
 
 		System.out.println("PRUEBA verempleados MYSQL");
